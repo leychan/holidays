632e59714cb26091e00835ac2e13f2dce546220b
lei <leilei1512159@gmail.com>
2019-12-06 14:27:10 +0800

    增加缓存, 提高性能

--- holidays.php
+++ holidays.php
@@ -10,11 +10,22 @@ require "vendor/autoload.php";
 
 use api\DateApi as DateApi;
 
+//新建一个 http server
 $server = new Swoole\Http\Server('0.0.0.0', 8080);
 
-$server->on('request', function($request, $response) {
+//缓存查询的节假日数据
+$cache = [];
+
+$server->on('request', function ($request, $response) {
     $date = isset($request->get['date']) ? $request->get['date'] : '';
+    if (empty($date)) {
+        $response->end(json_encode([
+            'status' => -1,
+            'error'  => 'date cannot be empty'
+        ]));
+        return;
+    }
     $date = new DateApi($date);
-    $response->end($date->isHoliday());
+    $response->end(json_encode($date->isHoliday()));
 });
 $server->start();
--- src/DateApi.php
+++ src/DateApi.php
@@ -15,9 +15,10 @@ class DateApi {
     public $date;
 
     public function __construct($date = '') {
+        echo 'real date ' . $date, PHP_EOL;
         $this->date = empty($date) ? date('Y-m-d') : $date;
         $this->dealDate();
-        $this->getSpecialData();
+        $this->holiday_data = $this->getSpecialData();
     }
 
     public function dealDate() {
@@ -26,33 +27,69 @@ class DateApi {
         $this->date_str = $date_arr[1] . $date_arr[2];
     }
 
-    public function getSpecialData() {
-        $path = __DIR__ . '/../data/' . $this->year . '_data.json';
-        if (!file_exists($path)) {
-            echo $this->year . ' data is not exist!';
-            exit();
+    /**
+     * 根据年份日期获取节假日数据, 获取以后存放在缓存数组中
+     */
+    public function getSpecialData() :array {
+        global $cache;
+        if (empty($cache[$this->year])) {
+            $path = __DIR__ . '/../data/' . $this->year . '_data.json';
+            if (!file_exists($path)) {
+                return [];
+            }
+            $data = file_get_contents($path);
+            $json = json_decode($data, true);
+            $cache[$this->year] = $json;
+            return $json;
         }
-        $data = file_get_contents($path);
-        $json = json_decode($data, true);
-        $this->holiday_data = $json;
+        return $cache[$this->year];
     }
 
-    public function isHoliday(): int {
+    /**
+     * 判断日期是否是假日/周末, 假日返回2, 周末返回1, 工作日返回0
+     *
+     * @return int
+     */
+    public function isHoliday() :array {
+        if (empty($this->holiday_data)) {
+            return [
+                'status' => -1,
+                'error'  => '没有查询的日期数据'
+            ];
+        }
         echo 'current deal date is ' . $this->date_str . PHP_EOL;
         $date = $this->getDate();
         echo 'date:' . $date . PHP_EOL;
         if (isset($this->holiday_data[$date])) {
-            echo 'loaded holidays data' . PHP_EOL;
-            return intval($this->holiday_data);
+            return [
+                'status'  => 0,
+                'error'   => '',
+                'holiday' => $this->holiday_data[$date]
+            ];
         }
-        return $this->normalDay($this->getWeekDay());
+        $holiday = $this->normalDay($this->getWeekDay());
+        return [
+            'status'  => 0,
+            'error'   => '',
+            'holiday' => $holiday
+        ];
     }
 
+    /**
+     * 获取月份和日期, 0901
+     *
+     * @return string
+     */
     public function getDate(): string {
-        echo 'date1' . $this->date . PHP_EOL;
         return empty($this->date_str) ? date('md') : date('md', strtotime($this->date));
     }
 
+    /**
+     * 判断是否是工作日, 如果周末则返回1, 否则返回0
+     *
+     * @param string $weekday
+     * @return int
+     */
     public function normalDay(string $weekday): int {
         if (in_array($weekday, ['0', '6'])) {
             return 1;
@@ -60,8 +97,13 @@ class DateApi {
         return 0;
     }
 
+    /**
+     * 获取日期是星期几
+     *
+     * @return string
+     */
     public function getWeekDay(): string {
-        echo $this->date . PHP_EOL;
+        echo 'this date '. $this->date . PHP_EOL;
         return empty($this->date_str) ? date('w') : date('w', strtotime($this->date));
     }
 }
